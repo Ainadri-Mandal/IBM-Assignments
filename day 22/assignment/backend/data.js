f3b73677c9672171188ed1a60342c9918e8dabcb
Ainadri Mandal <mandalainadri@gmail.com>
2025-03-20 12:41:43 +0530

    Update

---
+++ day 22/assignment/backend/data.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        id: "1",
+        name: "T-Shirt",
+        price: 500,
+        category: "Clothings"
+    },
+    {
+        id: "2",
+        name: "Pants",
+        price: 700,
+        category: "Clothings"
+    },
+    {
+        id: "3",
+        name: "Iphone",
+        price: 70000,
+        category: "Electronics"
+    },
+    {
+        id: "4",
+        name: "Toys",
+        price: 200,
+        category: "Kids Stuffs"
+    }
+];
---
+++ day 24/classwork/backend/configs/db.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const mongoURL = mongoose.connect(`mongodb+srv://ainadrimandal:ainadrimandal@cluster0.38ifs.mongodb.net/myDB?retryWrites=true&w=majority&appName=Cluster0`)
+
+exports.connectDB = mongoose.connect(mongoURL);
+
+// module.exports={
+//     connectDB
+// }
