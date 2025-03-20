a4541a2ba68146eac08a53190ee610b7ba68a8d6
Ainadri Mandal <mandalainadri@gmail.com>
2025-03-20 23:39:04 +0530

    Update

--- day 24/classwork/backend/configs/db.js
+++ day 24/classwork/backend/configs/db.js
@@ -1,7 +1,17 @@
-const mongoose = require("mongoose");
-const mongoURL = mongoose.connect(`mongodb+srv://ainadrimandal:ainadrimandal@cluster0.38ifs.mongodb.net/myDB?retryWrites=true&w=majority&appName=Cluster0`)
+const mongoose = require('mongoose');
+const connectDB = mongoose.connect(process.env.MongoURL);
+module.exports = {
+connectDB
+}
 
-exports.connectDB = mongoose.connect(mongoURL);
+
+
+
+
+// const mongoose = require("mongoose");
+// const mongoURL = mongoose.connect(`mongodb+srv://ainadrimandal:ainadrimandal@cluster0.38ifs.mongodb.net/myDB?retryWrites=true&w=majority&appName=Cluster0`)
+
+// exports.connectDB = mongoose.connect(mongoURL);
 
 // module.exports={
 //     connectDB
--- day 25/classwork/frontend/script.js
+++ day 25/classwork/frontend/script.js
@@ -12,20 +12,19 @@ document.getElementById('registerForm')?.addEventListener('submit', async(e) => 
     alert(data.message);
    });
    document.getElementById('loginForm')?.addEventListener('submit', async (e)=> {
-    e.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const res = await fetch('http://localhost:5000/api/auth/login', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ email, password })
-   });
-   const data = await res.json();
-   if (data.token) {
-   localStorage.setItem('token', data.token);
-   window.location.href = 'dashboard.html';
-   } else {
-   alert('Login failed');
-   }
-  });
-          
+e.preventDefault();
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+const res = await fetch('http://localhost:5000/api/auth/login', {
+method: 'POST',
+headers: { 'Content-Type': 'application/json' },
+body: JSON.stringify({ email, password })
+});
+const data = await res.json();
+if (data.token) {
+localStorage.setItem('token', data.token);
+window.location.href = 'dashboard.html';
+} else {
+alert('Login failed');
+}
+});
